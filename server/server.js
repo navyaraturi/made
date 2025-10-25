9b8ac5a86a7e181157d352b363f1c9392592fd23
Navya Raturi <codewithnavya@proton.me>
2025-10-25 21:20:45 +0000

    fallback to use BUN envs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import express from "express";
+import { Server } from "socket.io";
+
+const PORT = Bun.env.PORT || 3001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello there");
-});
-
-app.listen(3000, () => {
-  console.log(`server started on port 3000`);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
 
 console.log("application started");
